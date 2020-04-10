6d31e317666efa7bf28299f54149f71e39cddaaa
Abdullanz <amnjrx@gmail.com>
2020-04-10 19:51:46 +0300

    Working now!! but needs a lot of work

--- README.md
+++ README.md
@@ -2,38 +2,18 @@
 A simple 2D game that I am making to focus on the interactive aspect of storytelling, my goal here is to learn how interactive fiction works in general, then maybe expand from here to improve the story telling methodology and the graphics.  
 
 # Description
-This is a story about a young programmer who is defending himself from a series of obstacles.
-
-# PyGame Library
-PyGame is a Python library for gaming to deal with input, output functionality, and multimedia.
-
+This is a game that tells the story of a young hero who is defending the world from a series of challenges. I am currently using the graphics provided by the arcade game library, which uses the OpenGL library implementation.
 
 ## Installation
-Use the package manager [pip](https://pypi.org/project/pygame/) to install pygame.
-Use the package manager [pip](https://pypi.org/project/pyTMX/) to install pyTMX.
+[Arcade](https://pypi.org/project/arcade/)
 
 ```bash
-pip install pygame
-pip install pyTMX
+pip install arcade
 ```
 
 ## Usage
-Python Version 3.7
+Python Version 3.8
 
-```python
-import PyGame
-import pyTMX
-```
-
-## Roadmap
-My first step is to create a window, and a map on it. After that, I am going to add the couple of the characters and some of the dynamics of the gameplay. My third step, is going to be around developing the storytelling aspect, and the interactivity of the game itself. Finally, I am going to fix the issues within the game dynamics and release the first version.
-
-
-## Project status
-I just started working on couple of open-sourced projects, so I will be working on this game from time to time for fun.
-
-## Contributing
-Pull requests are welcomed. For changes, please open an issue first to discuss what you would like to change with me.
 
 ## Version
 0.1
--- source_code.py
+++ source_code.py
@@ -1,67 +1,341 @@
-# ###############################################################################
-# #  Description: Luna game with a simple interactive story
-# #  Author: Abdullah Najjar
-# #  Date: 4 March, 2020
-# #  Version: 0.1
-# ###############################################################################
-## NOTE: Experiment with tile maps seems fun!!
-
-import pygame as pg
+"""
+Author: Abdullah Najjar
+Version: 0.1
+Description: This game is called Adventure
+# TODO: Fix player speed and problem in movement, also screen size issues + add map tiling
+"""
+import arcade
 import os
-from options import options
-from Player import Player
-from pytmx import load_pygame #Library for tiled maps
+import random
+
+# Global variables
+SPRITE_SCALING = 0.5 #Character size
+SPRITE_SCALING_COIN = .50 #Size of coins
+COIN_COUNT = 50 #Number of coins
+
+SCREEN_WIDTH = 800 #Width of window
+SCREEN_HEIGHT = 600 #Height of window
+SCREEN_TITLE = "Adventure" #Window title
+
+MAX_SPEED = 2.7 # Speed limit
+ACCELERATION_RATE = 0.1 # How fast we accelerate
+FRICTION = 0.02 # How fast to slow down after we letr off the key
+
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = (SPRITE_PIXEL_SIZE * SPRITE_SCALING)
+
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+VIEWPORT_MARGIN = SPRITE_PIXEL_SIZE * SPRITE_SCALING
+RIGHT_MARGIN = 4 * SPRITE_PIXEL_SIZE * SPRITE_SCALING
+
+# Physics
+MOVEMENT_SPEED = 4 * SPRITE_SCALING
+JUMP_SPEED = 50 * SPRITE_SCALING
+GRAVITY = .5 * SPRITE_SCALING
 
 
-class source_code(object):
 
-   def __init__(self):
-      pg.init()
-      pg.mixer.init()
+class Player(arcade.Sprite):
+    """
+    Player class
+    """
+    def update(self):
+        """
+        Update for the movement of the player
+        # TODO: Make the screen edge move as the player moves
+        """
+        self.center_x += self.change_x
+        self.center_y += self.change_y
 
-    #Colors
-    white = (255, 255, 255)
-    green = (0, 255, 0)
-    blue = (0, 0, 128)
+        # Check to see if we hit the screen edge
+        if self.left < 0:
+            self.left = 0
+            self.change_x = 0 # Zero x speed
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+            self.change_x = 0
 
-    Screen_size = (800, 600)
-
-    Screen = pg.display.set_mode(Screen_size,pg.FULLSCREEN)
-
-
-
-    #Buttons -> Add the photos to images file
-    #Config_Button = [pygame.image.load(r"C:\Users\Config.png"),
-    #                    pygame.image.load(r"C:\Users\Config_Over.png")]
-    #Play_Button = [pygame.image.load(r"C:\Users\Play.png"),
-    #                pygame.image.load(r"C:\Users\Play_Over.png")]
-    #Exit_Button = [pygame.image.load(r"C:\Users\Exit.png"),
-    #                pygame.image.load(r"C:\Users\Exit_Over.png")]
-
-
-    # Set window name & font -> Change font
-    pg.display.set_caption('Luna')
-    font = pg.font.Font('freesansbold.ttf', 32)
+        if self.bottom < 0:
+            self.bottom = 0
+            self.change_y = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+            self.change_y = 0
 
 
-    # Maybe use as text bubble/ front page?
-    # A text suface object, on which text is drawn on it.
-    #text = font.render('Luna', True, green, blue)
-    # Create a rectangular object for the text surface object
-    #textRect = text.get_rect()
-    #textRect.center = (800 // 2, 600 // 2)
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__(width, height, title)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # Variables that will hold sprite lists
+        self.all_sprites_list = None
+        self.all_wall_list = None
+        self.static_wall_list = None
+        self.moving_wall_list = None
+        self.player_list = None
+        self.coin_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+        self.physics_engine = None
+        self.view_left = 0
+        self.view_bottom = 0
+        self.end_of_map = 0
+        self.game_over = False
+
+        self.score = 0
+
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
+        width, height = self.get_size()
+        self.set_viewport(0, width, 0, height)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.BABY_BLUE)
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists -> Player & coins
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.all_wall_list = arcade.SpriteList()
+        self.static_wall_list = arcade.SpriteList()
+        self.moving_wall_list = arcade.SpriteList()
+
+        self.score = 0    # Score
+
+        # Set up the player character image, size and position
+        self.player_sprite = Player(":resources:images/animated_characters/male_adventurer/maleAdventurer_walk7.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 2 * GRID_PIXEL_SIZE
+        self.player_sprite.center_y = 3 * GRID_PIXEL_SIZE
+        self.player_list.append(self.player_sprite)
+
+        # Create floor
+        for i in range(30):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", SPRITE_SCALING)
+            wall.bottom = 0
+            wall.center_x = i * GRID_PIXEL_SIZE
+            self.static_wall_list.append(wall)
+            self.all_wall_list.append(wall)
+
+        # Create platform side to side
+        wall = arcade.Sprite(":resources:images/tiles/grassMid.png", SPRITE_SCALING)
+        wall.center_y = 3 * GRID_PIXEL_SIZE
+        wall.center_x = 3 * GRID_PIXEL_SIZE
+        wall.boundary_left = 2 * GRID_PIXEL_SIZE
+        wall.boundary_right = 5 * GRID_PIXEL_SIZE
+        wall.change_x = 2 * SPRITE_SCALING
+
+        self.all_wall_list.append(wall)
+        self.moving_wall_list.append(wall)
+
+        # Create platform side to side
+        wall = arcade.Sprite(":resources:images/tiles/grassMid.png", SPRITE_SCALING)
+        wall.center_y = 3 * GRID_PIXEL_SIZE
+        wall.center_x = 7 * GRID_PIXEL_SIZE
+        wall.boundary_left = 5 * GRID_PIXEL_SIZE
+        wall.boundary_right = 9 * GRID_PIXEL_SIZE
+        wall.change_x = -2 * SPRITE_SCALING
+
+        self.all_wall_list.append(wall)
+        self.moving_wall_list.append(wall)
+
+        # Create platform moving up and down
+        wall = arcade.Sprite(":resources:images/tiles/grassMid.png", SPRITE_SCALING)
+        wall.center_y = 5 * GRID_PIXEL_SIZE
+        wall.center_x = 5 * GRID_PIXEL_SIZE
+        wall.boundary_top = 8 * GRID_PIXEL_SIZE
+        wall.boundary_bottom = 4 * GRID_PIXEL_SIZE
+        wall.change_y = 2 * SPRITE_SCALING
+
+        self.all_wall_list.append(wall)
+        self.moving_wall_list.append(wall)
+
+        # Create platform moving diagonally
+        wall = arcade.Sprite(":resources:images/tiles/grassMid.png", SPRITE_SCALING)
+        wall.center_y = 5 * GRID_PIXEL_SIZE
+        wall.center_x = 8 * GRID_PIXEL_SIZE
+        wall.boundary_left = 7 * GRID_PIXEL_SIZE
+        wall.boundary_right = 9 * GRID_PIXEL_SIZE
+
+        wall.boundary_top = 8 * GRID_PIXEL_SIZE
+        wall.boundary_bottom = 4 * GRID_PIXEL_SIZE
+        wall.change_x = 2 * SPRITE_SCALING
+        wall.change_y = 2 * SPRITE_SCALING
+
+        self.all_wall_list.append(wall)
+        self.moving_wall_list.append(wall)
+
+        self.physics_engine = \
+            arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                           self.all_wall_list,
+                                           gravity_constant=GRAVITY)
+
+        # Create the coins
+        for i in range(COIN_COUNT):
+
+            # Create the coin instance -> The Coin image from kenney.nl
+            coin = arcade.Sprite(":resources:images/items/coinGold.png",
+                                 SPRITE_SCALING_COIN)
+
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
+
+        # Set the viewport boundaries
+        # These numbers set where we have 'scrolled' to.
+        self.view_left = 0
+        self.view_bottom = 0
+
+        self.game_over = False
 
 
-    # Copying the text surface object to the display surface object at the center coordinate.
-    #    Screen.blit(text, textRect)
+    def on_draw(self):
+        """
+        Render the screen.
+        """
 
-    # Iterate over the list of Event objects
-    # that was returned by pygame.event.get() method.
-    for event in pg.event.get():
-        # if event object type is QUIT then quitting the pygame and program.
-        if event.type == pg.QUIT:
-            # deactivates pygame library
-            pg.quit()
-            quit()
-            # Draws the surface object to the screen.
-            pg.display.update()
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Get viewport dimensions
+        left, screen_width, bottom, screen_height = self.get_viewport()
+
+        self.coin_list.draw()
+        self.static_wall_list.draw()
+        self.moving_wall_list.draw()
+
+        # Put the text on the screen.
+        # Adjust the text position based on the viewport so that we don't
+        # scroll the text too.
+        # distance = self.player_sprite.right
+        # output = f"Distance: {distance}"
+        # arcade.draw_text(output, self.view_left + 10, self.view_bottom + 20,
+        #                  arcade.color.WHITE, 14)
+
+
+        # Put the text on the screen.
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+        self.player_list.draw()         # Draw all the sprites.
+
+        # Display speed
+        arcade.draw_text(f"X Speed: {self.player_sprite.change_x:6.3f}", 10, 50, arcade.color.WHITE)
+        arcade.draw_text(f"Y Speed: {self.player_sprite.change_y:6.3f}", 10, 70, arcade.color.WHITE)
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        self.coin_list.update()
+        self.physics_engine.update()
+
+        # Track if we need to change the viewport
+
+        changed = False
+        # Generate a list of all sprites that collided with the player.
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        # Loop through each colliding sprite, remove it, and add to the score.
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+
+        # Add some friction
+        if self.player_sprite.change_x > FRICTION:
+            self.player_sprite.change_x -= FRICTION
+        elif self.player_sprite.change_x < -FRICTION:
+            self.player_sprite.change_x += FRICTION
+        else:
+            self.player_sprite.change_x = 0
+
+        if self.player_sprite.change_y > FRICTION:
+            self.player_sprite.change_y -= FRICTION
+        elif self.player_sprite.change_y < -FRICTION:
+            self.player_sprite.change_y += FRICTION
+        else:
+            self.player_sprite.change_y = 0
+
+        # Apply acceleration based on the keys pressed
+        if self.up_pressed and not self.down_pressed:
+            self.player_sprite.change_y += ACCELERATION_RATE
+        elif self.down_pressed and not self.up_pressed:
+            self.player_sprite.change_y += -ACCELERATION_RATE
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x += -ACCELERATION_RATE
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x += ACCELERATION_RATE
+
+        if self.player_sprite.change_x > MAX_SPEED:
+            self.player_sprite.change_x = MAX_SPEED
+        elif self.player_sprite.change_x < -MAX_SPEED:
+            self.player_sprite.change_x = -MAX_SPEED
+        if self.player_sprite.change_y > MAX_SPEED:
+            self.player_sprite.change_y = MAX_SPEED
+        elif self.player_sprite.change_y < -MAX_SPEED:
+            self.player_sprite.change_y = -MAX_SPEED
+
+        # Call update to move the sprite
+        # If using a physics engine, call update on it instead of the sprite
+        # list.
+        self.player_list.update()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = JUMP_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+
+def main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
